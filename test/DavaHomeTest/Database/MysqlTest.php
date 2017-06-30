d39243d023a55eaffaf49e5ef4251e95227f60a2
thedava <thedava@users.noreply.github.com>
2017-07-01 01:49:23 +0200

    Added unittests for DirectValue, CustomOperator and both in combination

--- test/DavaHomeTest/Database/MysqlTest.php
+++ test/DavaHomeTest/Database/MysqlTest.php
@@ -2,6 +2,8 @@
 
 namespace DavaHomeTest\Database;
 
+use DavaHome\Database\CustomOperator;
+use DavaHome\Database\DirectValue;
 use DavaHome\Database\Mysql;
 
 class MysqlTest extends \PHPUnit_Framework_TestCase
@@ -50,6 +52,37 @@ class MysqlTest extends \PHPUnit_Framework_TestCase
         $result = $mysql->insert('foobar', ['foo' => 'bar']);
         $this->assertArrayHasKey('value_0', $result['params']);
         $this->assertEquals('INSERT INTO `foobar` SET `foo` = :value_0', $result['query']);
+        $this->assertEquals('bar', $result['params']['value_0']);
+    }
+
+    public function testUpdate_WithDirectValue()
+    {
+        $mysql = $this->getMysqlMock_Execute();
+
+        $result = $mysql->update('foobar', ['foo' => new DirectValue('BAR()')], ['foo' => new DirectValue('BAZ()')]);
+        $this->assertEmpty($result['params']);
+        $this->assertEquals('UPDATE `foobar` SET `foo` = BAR() WHERE `foo` = BAZ()', $result['query']);
+    }
+
+    public function testUpdate_WithCustomOperator()
+    {
+        $mysql = $this->getMysqlMock_Execute();
+
+        $result = $mysql->update('foobar', ['foo' => 'bar'], ['foo' => new CustomOperator('!=', 'baz')]);
+        $this->assertArrayHasKey('value_0', $result['params']);
+        $this->assertArrayHasKey('where_1', $result['params']);
+        $this->assertEquals('UPDATE `foobar` SET `foo` = :value_0 WHERE `foo` != :where_1', $result['query']);
+        $this->assertEquals('bar', $result['params']['value_0']);
+        $this->assertEquals('baz', $result['params']['where_1']);
+    }
+
+    public function testUpdate_WithCustomOperatorAndDirectValue()
+    {
+        $mysql = $this->getMysqlMock_Execute();
+
+        $result = $mysql->update('foobar', ['foo' => 'bar'], ['foo' => new CustomOperator('!=', new DirectValue('BAZ()'))]);
+        $this->assertArrayHasKey('value_0', $result['params']);
+        $this->assertEquals('UPDATE `foobar` SET `foo` = :value_0 WHERE `foo` != BAZ()', $result['query']);
         $this->assertEquals('bar', $result['params']['value_0']);
     }
 }
